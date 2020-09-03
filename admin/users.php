ffe0fc04bdd18932c414225e3f748edc9c7b2d3d
Sepand JamshidPour <sepand100@gmail.com>
2020-09-03 22:01:14 +0430

    created show users page

--- admin/users.php
+++ admin/users.php
@@ -3,4 +3,112 @@
 require_once('../includes/config.php');
 
 //if not logged in redirect to login page
-if(!$user->is_logged_in()){ header('Location: login.php'); }
+if(!$user->is_logged_in())
+{
+    header('Location: login.php');
+}
+
+//show message from add / edit page
+if (isset($_GET['delUser']))
+{
+    //if user id is 1 ignore
+    //because it admin
+    if ($_GET['delUser'] != 1)
+    {
+        $sql = 'DELETE FROM blog_members WHERE memberID = :memberID';
+        $stm = $db->prepare($sql);
+        $stm->bindParam(':memberID', $_GET['delUser']);
+        $stm->execute();
+
+        header('Location: users.php?action=deleted');
+        exit();
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Admin | Users</title>
+</head>
+
+<body>
+
+<?php include 'menu.php';
+
+//show message from add / edit page
+if (isset($_GET['action']))
+{
+    echo '<h3>User '.$_GET['action'].' </h3>';
+}
+?>
+
+
+<div>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+        </tr>
+
+<?php
+
+try {
+    $sql = 'SELECT memberID, username, email FROM blog_members ORDER BY username';
+    $result = $db->prepare($sql);
+    $result->execute();
+
+    while ($members = $result->fetch(PDO::FETCH_ASSOC))
+    {
+        echo '<tr>';
+        echo '<td>'.$members['memberID'].'</td>';
+        echo '<td>'.$members['username'].'</td>';
+        echo '<td>'.$members['email'].'</td>';
+        ?>
+
+        <!--
+        action field
+        edit and delete
+
+        if id=1 dont show delete
+        -->
+        <td>
+            <a href="edit_user.php?id=<?php echo $members['memberID'];?>">Edit</a>
+            <?php
+                if ($members['memberID'] != 1)
+            { ?>
+              | <a href="javascript:delUser(<?php echo $members['memberID'];?>,<?php echo $members['username'];?>)">Delete</a>
+            <?php } ?>
+        </td>
+
+        <?php
+        echo '</tr>';
+    }
+}
+
+catch (PDOException $e)
+{
+    $e->getMessage();
+}
+
+?>
+
+    </table>
+</div>
+
+<a href="add_user.php">Add User</a>
+
+<script type="text/javascript">
+    function delUser(id, title) {
+        if (confirm("Are you sure you want to delete '" + title + "'"))
+        {
+            window.location.href = 'users.php?delUser=' + id;
+        }
+    }
+</script>
+
+</body>
+</html>
