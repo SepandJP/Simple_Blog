18fc85ed0fe39ec73a0cdd764a352928762c2fd9
Sepand JamshidPour <sepand100@gmail.com>
2020-10-28 12:30:46 +0330

    edit menu grid size

--- admin/menu.php
+++ admin/menu.php
@@ -3,13 +3,13 @@
 <!--    <p>Logged in as --><?php //=$_SESSION['username'];?><!--</p>-->
 </header>
 
-<div class="col-sm-2 bg-dark h-auto">
+<div class="col-sm-3 col-lg-2 bg-dark">
 
     <p class="navbar-brand text-white font-weight-bold">Menu</p>
 
     <nav class="align-middle">
         <ul class="nav flex-column">
-            <li class="nav-item active m-1">
+            <li class="nav-item m-1">
                 <a href="index.php" class="nav-link btn btn-primary">Main</a>
             </li>
             <li class="nav-item m-1">
@@ -19,7 +19,7 @@
                 <a href="users.php" class="nav-link btn btn-primary">Users</a>
             </li>
             <li class="nav-item m-1">
-                <a href="../" class="nav-link btn btn-primary">View Blog</a>
+                <a href="../" class="px-0 nav-link btn btn-primary">View Blog</a>
             </li>
             <li class="nav-item m-1">
                 <a href="logout.php" class="nav-link btn btn-primary">Log out</a>
