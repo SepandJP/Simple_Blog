6eb2755a8e8a24e9554cbca069840ecbf59185b2
Sepand JamshidPour <sepand100@gmail.com>
2020-10-30 18:47:07 +0330

    add edit_user page frontend

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -16,12 +16,15 @@ if(!$user->is_logged_in()){
     <title>Admin | Edit User</title>
 </head>
 
-<body>
+<body class="bg-dark">
+
+<div class="container-fluid row m-0 p-0">
 
 <?php include 'menu.php';?>
 
-<p><a href="users.php">User Admin Index</a></p>
-<h2>Edit User</h2>
+<div class="col-sm-9 col-lg-10">
+
+<h1 class="display-4 text-white">Edit User</h1>
 
 <?php
 
@@ -113,7 +116,12 @@ if (isset($_POST['submit']))
 if (isset($error))
 {
     foreach ($error as $err) {
-        echo '<p>'.$err.'</p>';
+        echo '<div class="alert alert-danger alert-dismissible fade show">';
+        echo '<span>'.$err.'</span>';
+        echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>';
+        echo '</div>';
     }
 }
 
@@ -133,36 +141,39 @@ catch (PDOException $e)
 }
 ?>
 
-<div>
+<div class="jumbotron">
     <form method="post">
         <input type="hidden" name="memberID" value="<?php echo $userInfo['memberID'];?>">
 
-        <label for="memberUsername">Username
-            <input type="text" name="username" id="memberUsername" placeholder="Username" value="<?php echo $userInfo['username'];?>">
-        </label>
+        <div class="form-group">
+            <label for="memberUsername">Username
+                <input class="form-control" type="text" name="username" id="memberUsername" placeholder="Username" value="<?php echo $userInfo['username'];?>">
+            </label>
+        </div>
 
-        <br />
+        <div class="form-group">
+            <label for="memberEmail">Email
+                <input class="form-control" type="email" name="email" id="memberEmail" placeholder="Email" value="<?php echo $userInfo['email'];?>">
+            </label>
+        </div>
 
-        <label for="memberEmail">Email
-            <input type="email" name="email" id="memberEmail" placeholder="Email" value="<?php echo $userInfo['email'];?>">
-        </label>
+        <div class="form-group">
+            <label for="memberPassword">Password
+                <input class="form-control" type="password" name="password" id="memberPassword" placeholder="Password">
+            </label>
+        </div>
 
-        <br />
-
-        <label for="memberPassword">Password (only to change)
-            <input type="password" name="password" id="memberPassword" placeholder="Password">
-        </label>
-
-        <br />
-
-        <label for="memberPasswordConfirm">Confirm Password
-            <input type="password" name="passwordConfirm" id="memberPasswordConfirm" placeholder="Confirm Password">
-        </label>
-
-        <br />
+        <div class="form-group">
+            <label for="memberPasswordConfirm">Confirm Password
+                <input class="form-control" type="password" name="passwordConfirm" id="memberPasswordConfirm" placeholder="Confirm Password">
+            </label>
+        </div>
 
         <input type="submit" name="submit" value="Update User">
     </form>
+</div>
+
+</div>
 </div>
 
 <!-- JavaScript files -->
